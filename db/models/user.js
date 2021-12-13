f1a619348a86eaa9b507ea47f52290671c911e66
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2021-12-13 14:17:31 -0800

    Fixed user associations

--- db/models/user.js
+++ db/models/user.js
@@ -18,8 +18,8 @@ module.exports = (sequelize, DataTypes) => {
   },{});
   User.associate = function(models) {
     // associations can be defined here
-    User.belongsToMany(models.User, {foreignKey: 'userId', through: 'Contact', otherKey: 'contactId'})
-    User.belongsToMany(models.User, {foreignKey: 'contactId', through: 'Contact', otherKey: 'userId'})
+    User.belongsToMany(models.User, {foreignKey: 'userId', through: 'Contact', otherKey: 'contactId', as: 'contacts'})
+    User.belongsToMany(models.User, {foreignKey: 'contactId', through: 'Contact', otherKey: 'userId', as: 'contactees'})
     User.hasMany(models.Comment, {foreignKey: 'userId'});
     User.hasMany(models.Task, {foreignKey: 'userId'});
     User.hasMany(models.Task, {foreignKey: 'givenTo'});
