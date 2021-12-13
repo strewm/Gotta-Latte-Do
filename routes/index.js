182c20c6989fb1585c7b692817192d1c1f33d090
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2021-12-13 15:30:43 -0800

    Redirect if user is logged in added to index route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (res.locals.authenticated) {
+    return res.redirect('/app')
+  }
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
