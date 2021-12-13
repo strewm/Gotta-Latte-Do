35255729ce1f0463d7356a0c7e5441fecd708088
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2021-12-13 13:29:11 -0800

    Added utils.js to root directory with asyncHandler, handleValidationErrors, and csrfProtection

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+const { restoreUser } = require('./auth');
+
 const app = express();
 
 // view engine setup
@@ -35,6 +37,7 @@ app.use(
 // create Session table if it doesn't already exist
 store.sync();
 
+app.use(restoreUser);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
