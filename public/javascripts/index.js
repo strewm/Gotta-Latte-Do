267e23c8b41bd2d0cf0bb3b1c239bf5ab6d322e5
Savanah Trewman <strewman@me.com>
2021-12-13 17:28:35 -0600

    figuring out the frontend

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,13 +4,17 @@
 
 document.addEventListener("DOMContentLoaded", async() => {
     try {
-        const res = await fetch("http://localhost:8080/app/#all", {
+        const res = await fetch("/app", {
             headers: {}
         });
 
+        if (res.locals.authenticated) {
+            return res.redirect('/app');
+        }
+
         // Load elements of home page
         const { tasks } = await res.json();
-        
+
         // Redirect un-authenticated users
         if (res.status === 401) {
             window.location.href = '/not-found';
