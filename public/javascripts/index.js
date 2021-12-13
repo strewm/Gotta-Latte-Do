1cbcc556f044d5a80c790d2cdc19c5395955ffbc
Savanah Trewman <strewman@me.com>
2021-12-13 16:01:09 -0600

    working on public > javascripts > index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,22 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
-})
+// window.addEventListener("load", (event)=>{
+//     console.log("hello from javascript!")
+// })
+
+document.addEventListener("DOMContentLoaded", async() => {
+    try {
+        const res = await fetch("http://localhost:8080/app/#all", {
+            headers: {}
+        });
+
+        // Load elements of home page
+        const { tasks } = await res.json();
+        
+        // Redirect un-authenticated users
+        if (res.status === 401) {
+            window.location.href = '/not-found';
+            return;
+        }
+    } catch (e) {
+        console.error(e);
+    }
+});
