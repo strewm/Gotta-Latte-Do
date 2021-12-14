8866f657b1560a561f5e566692ea95af44adaf23
Tanner Shaw <traumaaa@gmail.com>
2021-12-13 16:11:49 -0800

    Created a task validator, and updated the post route

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,13 +6,25 @@ const { asyncHandler, csrfProtection, handleValidationErrors } = require('../uti
 const { Task, User } = db;
 
 
+const validateTask = [
+  check('description')
+  .exists({checkFalsy: true})
+  .withMessage('Must provide a description for task'),
+  check('dueDate')
+  .exists({checkFalsy: true})
+  .withMessage('Must provide a due date for task')
+  .isISO8601()
+  .withMessage('Must provide a valid date')
+]
+
+
 router.get('/users/:id(\\d+)/tasks', csrfProtection, asyncHandler(async(req, res) => {
   const tasks = await Task.findAll({where: {userId: req.params.id}});
   res.json({tasks})
 }))
 
 
-router.post('/users/:id(\\d+)/tasks', csrfProtection, asyncHandler(async(req, res) => {
+router.post('/users/:id(\\d+)/tasks', validateTask, handleValidationErrors, csrfProtection, asyncHandler(async(req, res) => {
   const { description, dueDate, givenTo } = req.body;
   const userId = res.locals.userId
   const task = await Task.create({
