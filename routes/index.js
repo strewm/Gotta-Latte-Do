5299ec6c1467bcac5bb6c1275751d63f30cbe480
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2021-12-13 16:33:55 -0800

    Added router.get('/app')

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const { csrfProtection, asyncHandler, handleValidationErrors } = require("../utils");
+
 var router = express.Router();
 
 /* GET home page. */
@@ -6,7 +8,11 @@ router.get('/', function(req, res, next) {
   if (res.locals.authenticated) {
     return res.redirect('/app')
   }
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+  res.render('index', { title: 'Gotta Latte Do' });
 });
+
+router.get('/app', csrfProtection, function(req, res, next) {
+  res.render('app', { csrfToken: req.csrfToken() })
+})
 
 module.exports = router;
