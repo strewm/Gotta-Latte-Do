88b1f6bfab22544d650b1f31b3d9d99a6f1a0e4e
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2021-12-14 09:48:29 -0800

    Comments API route handlers

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const taskRouter = require('./routes/tasks')
+const commentsRouter = require('./routes/comments')
 
 const { restoreUser } = require('./auth');
 const { AnimationObjectGroup } = require('three');
@@ -42,7 +43,8 @@ store.sync();
 app.use(restoreUser);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/tasks', taskRouter)
+app.use('/tasks', taskRouter);
+app.use(commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/comments.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const db = require('../db/models');
+const { asyncHandler, handleValidationErrors } = require('../utils');
+const { Task, User, Comment, Contact } = db;
+
+
+const validateComment =
+    check('message')
+    .exists({ checkFalsy: true })
+    .withMessage('Must provide a comment in order to submit.')
+
+router.get('/tasks/:id(\\d+)/comments', asyncHandler(async (req, res) => {
+    const comments = await Comment.findAll({
+        where: {
+            userId: res.locals.userId,
+            taskId: req.params.id
+        }
+    })
+
+    res.json({ comments })
+}))
+
+router.post(
+    '/tasks/:id(\\d+)/comments',
+    validateComment,
+    handleValidationErrors,
+    asyncHandler(async (req, res) => {
+        const { message } = req.body;
+        const userId = res.locals.userId;
+        const taskId = req.params.id;
+        const comment = await Comment.create({
+            userId,
+            taskId,
+            message
+        })
+        res.status(201).json({ comment });
+    }))
+
+router.put(
+    '/comments/:id(\\d+)',
+    validateComment,
+    handleValidationErrors,
+    asyncHandler(async (req, res) => {
+        const { message } = req.body;
+        const comment = await Comment.findByPk(req.params.id);
+        await comment.update({ message });
+        res.json({ comment });
+    }
+))
+
+router.delete(
+    '/comments/:id(\\d+)',
+    asyncHandler(async (req, res) => {
+        const comment = await Comment.findByPk(req.params.id);
+        await comment.destroy();
+        res.status(204).end();
+    })
+)
+
+
+
+
+
+
+
+
+
+
+module.exports = router
