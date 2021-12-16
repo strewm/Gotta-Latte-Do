d72bd2d259097f5592820bf85e3a07666f79ce1b
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2021-12-16 15:19:37 -0800

    LIST ADD NOW WORKS WITH NO ERRORS, and clicking on list shows tasks for list. FIXED BUG.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -240,35 +240,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
 
-    const listLists = document.querySelectorAll('.list-lists');
-
-    listLists.forEach((list) => {
-      list.addEventListener('click', async(e) => {
-        e.stopPropagation();
-        const listId = e.target.id;
-        console.log(listId)
-
-        const res = await fetch(`/lists/${listId}/tasks`);
-
-        const { tasks } = await res.json();
-
-        const tasksContainer = document.querySelector('.task-list');
-
-        const listTitle = `
-          <h2 class="task-list-header">${tasks[0].List.title}</h2>
-        `
-
-        const tasksHtml = tasks.map((task) => `
-          <div class="task-info">
-            <input type="checkbox" class="task-check-box" id=${task.Task.id} name=${task.Task.id}>
-            <label for=${task.Task.id} id=${task.Task.id} class="task-check-box">${task.Task.description}</label>
-          </div>
-        `)
-
-        tasksContainer.innerHTML = listTitle + tasksHtml.join('');
-      })
-    })
-
 
 
     const tasksListContainer = document.querySelector(".task-list");
@@ -503,6 +474,35 @@ const fetchLists = async () => {
   `)
 
   listContainer.innerHTML = listHtml.join("");
+
+  const listLists = document.querySelectorAll('.list-lists');
+
+    listLists.forEach((list) => {
+      list.addEventListener('click', async(e) => {
+        e.stopPropagation();
+        const listId = e.target.id;
+        console.log(listId)
+
+        const res = await fetch(`/lists/${listId}/tasks`);
+
+        const { tasks } = await res.json();
+
+        const tasksContainer = document.querySelector('.task-list');
+
+        const listTitle = `
+          <h2 class="task-list-header">${tasks[0].List.title}</h2>
+        `
+
+        const tasksHtml = tasks.map((task) => `
+          <div class="task-info">
+            <input type="checkbox" class="task-check-box" id=${task.Task.id} name=${task.Task.id}>
+            <label for=${task.Task.id} id=${task.Task.id} class="task-check-box">${task.Task.description}</label>
+          </div>
+        `)
+
+        tasksContainer.innerHTML = listTitle + tasksHtml.join('');
+      })
+    })
 }
 
 
@@ -600,17 +600,22 @@ addList.addEventListener('click', (e) => {
       e.stopPropagation();
       e.preventDefault();
       const formData = new FormData(addList);
-      const createList = formData.get('title');
-      const body = { createList };
-      await fetch(`http://localhost:8080/lists`, {
-          method: 'POST',
-          body: JSON.stringify(body),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-        addListForm.innerHTML = '';
-        await fetchLists();
+      const title = formData.get('title');
+      const body = { title };
+
+      try {
+        await fetch(`/lists`, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+          addListForm.innerHTML = '';
+          await fetchLists();
+      } catch(e) {
+        console.error(e);
+      }
       });
     const addCancelButton = document.querySelector('.listCancelButton');
 
