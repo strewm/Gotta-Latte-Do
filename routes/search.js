d6a6fdb3d5b7543630a687ac47ded5be9bea4021
Thien Dang <ctap105@gmail.com>
2021-12-16 17:15:43 -0500

    phase 2 search function not completed

--- routes/search.js
+++ routes/search.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { check } = require('express-validator');
 const db = require('../db/models');
 const { asyncHandler, csrfProtection, handleValidationErrors } = require('../utils');
-const { Task, TaskList } = db;
+const { Task, TaskList, List } = db;
 
-router.get('/:id', asyncHandler(async (req, res) => {
+router.get('/:id', asyncHandler(async (req, res, next) => {
     const searchQuery = req.params.id;
     const firstThree = searchQuery.slice(0, 3);
     const firstThreeUp = firstThree.charAt(0).toUpperCase() + firstThree.slice(1);
@@ -14,7 +14,7 @@ router.get('/:id', asyncHandler(async (req, res) => {
 
     const userId = res.locals.userId;
     try{
-        const results = await Task.findAll({
+        const resultsUncat = await Task.findAll({
             where: {
                 userId,
                 [Op.or]: [
@@ -26,13 +26,36 @@ router.get('/:id', asyncHandler(async (req, res) => {
                     {description: { [Op.iLike]: `${lastThree}`}},
                 ]
 
+            },
+            include: {
+                model: List,
             }
         })
 
-        res.send(results)
+        const results = await TaskList.findAll({
+            include: [{
+                model: Task,
+                //through: {
+                    where: {
+                        userId,
+                        [Op.or]: [
+                            {description: { [Op.substring]: `${searchQuery}`}},
+                            {description: { [Op.substring]: `${firstThree}`}},
+                            {description: { [Op.substring]: `${lastThree}`}},
+                            {description: { [Op.substring]: `${firstThreeUp}`}},
+                            {description: { [Op.iLike]: `${firstThree}`}},
+                            {description: { [Op.iLike]: `${lastThree}`}},
+                        ]
+
+                    }
+                //}
+            }, List]
+        })
+
+        res.status(201).json({results, resultsUncat})
 
     } catch (e) {
-        console.log('oops')
+        next(e)
     }
 
 
