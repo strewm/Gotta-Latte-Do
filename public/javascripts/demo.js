3701bfb3e4769465be78aed92f3a3afdf285b8f7
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2021-12-18 22:18:19 -0800

    comments to demo.js and refactored

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -1,34 +1,6 @@
+import { handleErrors } from './utils.js';
 
-const handleErrors = async (err) => {
-  if (err.status >= 400 && err.status < 600) {
-    const errorJSON = await err.json();
-    const errorsContainer = document.querySelector(".errors-container");
-    let errorsHtml = [
-      `
-        <div class="alert alert-danger">
-            Something went wrong. Please try again.
-        </div>
-      `,
-    ];
-    const { errors } = errorJSON;
-    if (errors && Array.isArray(errors)) {
-      errorsHtml = errors.map(
-        (message) => `
-          <div class="alert alert-danger">
-              ${message}
-          </div>
-        `
-      );
-    }
-    errorsContainer.innerHTML = errorsHtml.join("");
-  } else {
-    alert(
-      "Something went wrong. Please check your internet connection and try again!"
-    );
-  }
-};
-
-
+// Add functionality to demo user button to log in as a demo user
 const demoButton = document.querySelector('.demo-user');
 demoButton.addEventListener('click', async(e) => {
   e.preventDefault();
