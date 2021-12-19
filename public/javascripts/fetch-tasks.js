5acdcb54db937f513556bfd683606542c1b0ff7b
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2021-12-18 22:19:47 -0800

    pretty-fied fetch-tasks.js

--- public/javascripts/fetch-tasks.js
+++ public/javascripts/fetch-tasks.js
@@ -1,30 +1,29 @@
 import { addTaskInfoListeners } from "./utils.js";
-import { handleErrors } from './utils.js';
 
 // fetch user's tasks (all)
 export const fetchTasks = async () => {
-    const res = await fetch("/tasks")
+  const res = await fetch("/tasks")
 
-    if (res.status === 401) {
-        window.location.href = "/log-in";
-        return;
-      }
+  if (res.status === 401) {
+    window.location.href = "/log-in";
+    return;
+  }
 
-    const { tasks, user } = await res.json();
+  const { tasks, user } = await res.json();
 
-    const tasksListContainer = document.querySelector(".task-list");
-    const listName = `
+  const tasksListContainer = document.querySelector(".task-list");
+  const listName = `
   <h2 class="task-list-header">All of <strong>${user.username}'s</strong> self-assigned tasks.</h2>
   `
-    const tasksHtml = tasks.map(({ id, description, isCompleted }) => {
-      if (isCompleted === true) {
-        return `
+  const tasksHtml = tasks.map(({ id, description, isCompleted }) => {
+    if (isCompleted === true) {
+      return `
         <div class='task-info' id=${id}>
             <input type="checkbox" class="task-check-box" id=${id} name=${id} checked>
             <label for=${id} id=${id} class="task-check-box">${description}</label>
         </div>
         `
-      } else {
+    } else {
       return `<div class='task-info' id=${id}>
                 <input type="checkbox" class="task-check-box" id=${id} name=${id}>
                 <label for=${id} id=${id} class="task-check-box">${description}</label>
@@ -34,28 +33,26 @@ export const fetchTasks = async () => {
   })
   tasksListContainer.innerHTML = listName + tasksHtml.join("");
   await addTaskInfoListeners();
-  }
-
+}
 
 
 // fetch assigned tasks
-
 export const fetchAssignTasks = async () => {
-    const res = await fetch("/tasks/assigned")
+  const res = await fetch("/tasks/assigned")
 
-    if (res.status === 401) {
-      window.location.href = "/log-in";
-      return;
-    }
+  if (res.status === 401) {
+    window.location.href = "/log-in";
+    return;
+  }
 
-    const { tasks } = await res.json();
+  const { tasks } = await res.json();
 
-    const assignedTaskContainer = document.querySelector(".assigned-list");
-    if(tasks) {
-      const listName = `
+  const assignedTaskContainer = document.querySelector(".assigned-list");
+  if (tasks) {
+    const listName = `
       <h2 class="task-list-header">All tasks assigned to <strong></strong> by others.</h2>
       `
-      const tasksHtml = tasks.map(({ id, description, User }) => `
+    const tasksHtml = tasks.map(({ id, description, User }) => `
       <div class="assigned-grid">
       <div class="task-info" id=${id}>
           <input type="checkbox" class="task-check-box" id=${id} name=${id}>
@@ -67,39 +64,38 @@ export const fetchAssignTasks = async () => {
       </div>
       `)
 
-      assignedTaskContainer.innerHTML = listName + tasksHtml.join("")
+    assignedTaskContainer.innerHTML = listName + tasksHtml.join("")
 
-      await addTaskInfoListeners();
-    }
-
+    await addTaskInfoListeners();
   }
 
+}
 
-  // fetch user's tasks (all)
 
+// fetch user's incomplete tasks
 export const fetchIncompleteTasks = async () => {
-    const res = await fetch("/tasks/incomplete")
+  const res = await fetch("/tasks/incomplete")
 
-    if (res.status === 401) {
-        window.location.href = "/log-in";
-        return;
-      }
+  if (res.status === 401) {
+    window.location.href = "/log-in";
+    return;
+  }
 
-    const { tasks, user } = await res.json();
+  const { tasks, user } = await res.json();
 
-    const tasksListContainer = document.querySelector(".task-list");
-    const listName = `
+  const tasksListContainer = document.querySelector(".task-list");
+  const listName = `
     <h2 class="task-list-header"><strong>${user.username}'s</strong> incomplete tasks.</h2>
     `
-    const tasksHtml = tasks.map(({ id, description, isCompleted }) => {
-      if (isCompleted === true) {
-        return `
+  const tasksHtml = tasks.map(({ id, description, isCompleted }) => {
+    if (isCompleted === true) {
+      return `
         <div class='task-info' id=${id}>
             <input type="checkbox" class="task-check-box" id=${id} name=${id} checked>
             <label for=${id} id=${id} class="task-check-box">${description}</label>
         </div>
         `
-      } else {
+    } else {
       return `<div class='task-info' id=${id}>
                 <input type="checkbox" class="task-check-box" id=${id} name=${id}>
                 <label for=${id} id=${id} class="task-check-box">${description}</label>
@@ -108,37 +104,36 @@ export const fetchIncompleteTasks = async () => {
     }
   })
 
-    tasksListContainer.innerHTML = listName + tasksHtml.join("");
+  tasksListContainer.innerHTML = listName + tasksHtml.join("");
 
-    await addTaskInfoListeners();
-  }
+  await addTaskInfoListeners();
+}
 
 
 // fetch user's completed tasks
-
 export const fetchCompletedTasks = async () => {
-    const res = await fetch("/tasks/complete")
+  const res = await fetch("/tasks/complete")
 
-    if (res.status === 401) {
-        window.location.href = "/log-in";
-        return;
-      }
+  if (res.status === 401) {
+    window.location.href = "/log-in";
+    return;
+  }
 
-    const { tasks, user } = await res.json();
+  const { tasks, user } = await res.json();
 
-    const tasksListContainer = document.querySelector(".task-list");
-    const listName = `
+  const tasksListContainer = document.querySelector(".task-list");
+  const listName = `
     <h2 class="task-list-header"><strong>${user.username}'s</strong> completed tasks.</h2>
     `
-    const tasksHtml = tasks.map(({ id, description, isCompleted }) => {
-      if (isCompleted === true) {
-        return `
+  const tasksHtml = tasks.map(({ id, description, isCompleted }) => {
+    if (isCompleted === true) {
+      return `
         <div class='task-info' id=${id}>
             <input type="checkbox" class="task-check-box" id=${id} name=${id} checked>
             <label for=${id} id=${id} class="task-check-box">${description}</label>
         </div>
         `
-      } else {
+    } else {
       return `<div class='task-info' id=${id}>
                 <input type="checkbox" class="task-check-box" id=${id} name=${id}>
                 <label for=${id} id=${id} class="task-check-box">${description}</label>
@@ -147,7 +142,7 @@ export const fetchCompletedTasks = async () => {
     }
   })
 
-    tasksListContainer.innerHTML = listName + tasksHtml.join("");
+  tasksListContainer.innerHTML = listName + tasksHtml.join("");
 
-    await addTaskInfoListeners();
-  }
+  await addTaskInfoListeners();
+}
