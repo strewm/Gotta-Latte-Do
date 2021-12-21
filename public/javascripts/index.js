3052a6cd30291569a32a72db40ecd819f8a76491
Savanah Trewman <strewman@me.com>
2021-12-20 20:05:04 -0600

    working on the js for the splash page carousel

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,20 @@
 window.addEventListener("load", (event)=>{
 
 })
+
+const slidesContainer = document.querySelector('.slides-container');
+const slides = slidesContainer.children;
+const slidesToo = Array.from(slidesContainer.children);
+
+console.log(slides);
+console.log(slidesToo);
+
+const leftButt = document.querySelector('.left-button');
+const rightButt = document.querySelector('.right-button');
+
+const circleButts = document.querySelector('.circle-butts');
+const circles = circleButts.children;
+console.log(circles);
+
+// Get the width of the current slide
+const slidesWidth = slides[0].getBoundingClientRect().width;
