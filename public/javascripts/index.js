073771c0b3a8ac29e99a646df9139b7a4dc99d43
Savanah Trewman <strewman@me.com>
2021-12-26 13:30:46 -0600

    finished the second version of splash page carousel, but issue with onclick not recognizing the functions in index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,47 @@
-window.addEventListener("load", (event)=>{
+// Setting up index of slides variable
+var slideIdx = 1;
+viewSlide(slideIdx);
 
-})
+// Left/right buttons
+function changeSlide(shift) {
+    viewSlide(slideIdx += shift)
+};
+
+// Slide shifting
+function currSlide(index) {
+    changeSlide(slideIdx = index);
+};
+
+function viewSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName('slides');
+    var circles = document.getElementsByClassName('circle');
+    console.log(slides);
+    console.log(circles);
+
+    // If user clicks right on last slide, will bring the user
+    //  back to first slide
+    if (n > slides.length) {
+        slideIdx = 1;
+    }
+
+    // If user clicks left on first slide, will bring the user
+    //  to last slide
+    if (n < 1) {
+        slideIdx = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < circles.length; i++) {
+        circles[i].className = circles[i].className.replace("active", "");
+
+        // circles[i].className += "active";
+    }
+
+    slides[slideIdx-1].style.display = "block";
+    circles[slideIdx-1].className += "active";
+};
+
